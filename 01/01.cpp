7e11c01d65b7559e0cf2213e945c66a0d7e7fdb0
zhenxiong jiang <jiangzhenxiong@outlook.com>
2021-08-15 11:58:08 +0800

    添加项目文件。

---
+++ 01/01.cpp
@@ -0,0 +1,38 @@
+﻿
+
+#include <stdio.h>
+#include <pthread.h>
+#include <conio.h>
+
+
+#pragma comment(lib, "pthreadVC2.lib")
+
+using namespace std;
+
+#define NUM_THREADS 5
+
+// 线程的运行函数
+void* say_hello(void* args)
+{
+    printf("牛逼普拉斯\n\n");
+    printf("\nhello wo hello wo \n");
+    return 0;
+}
+
+int main()
+{
+    // 定义线程的 id 变量，多个变量使用数组
+    pthread_t tids[NUM_THREADS];
+    for (int i = 0; i < NUM_THREADS; ++i)
+    {
+        //参数依次是：创建的线程id，线程参数，调用的函数，传入的函数参数
+        int ret = pthread_create(&tids[i], NULL, say_hello, NULL);
+        if (ret != 0)
+        {
+            printf("创建线程错误！！");
+        }
+    }
+    //等各个线程退出后，进程才结束，否则进程强制结束了，线程可能还没反应过来；
+    pthread_exit(NULL);
+
+}
